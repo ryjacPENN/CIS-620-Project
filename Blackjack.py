403844fe1d4ce8888eb253ad6fb90931570e1610
ryjacPENN <rjacobow@seas.upenn.edu>
2023-10-28 17:06:49 -0400

    Added print functions

--- Blackjack.py
+++ Blackjack.py
@@ -26,11 +26,13 @@ class BlackJackGame:
         self.player_money = [initial_money] * num_players
 
         self.current_player = 0
+        self.current_turn = 0
 
     def Shuffle(self):
         self.deck = DeckCreator()
 
     def NextTurn(self):
+        self.current_turn += 1
         self.Shuffle()
 
         for i in range(len(self.player_cards)):
@@ -58,12 +60,33 @@ class BlackJackGame:
         else:
             return False
 
+    def DisplayCard(self, player_num, card_num):
+        card_name = ""
+        if player_num == -1:
+            card_name = str(self.dealer_cards[card_num][1]) + " of " + self.dealer_cards[card_num][0]
+        else:
+            card_name = str(self.player_cards[player_num][card_num][1]) + " of " + self.player_cards[player_num][card_num][0]
+
+        return card_name
+
+    def DisplayCurrentGameState(self):
+        print("##################################")
+        print("Turn", self.current_turn)
+        print("Dealer Cards :", self.DisplayCard(-1, 1), "and ?") # Hidden card is 0 index, revealed card is 1 index
+
+        for p in range(len(self.player_cards)):
+            print("Player", (p + 1), "Cards :", self.DisplayCard(p, 0), "and", self.DisplayCard(p, 1), ": $" + str(self.player_money[p]))
+
+        print("Player " + str(self.current_player + 1) + "'s turn: ")
+
     def PlayBlackJack(self):
+        # Start game to initial configuration
         self.NextTurn()
+
+        # Print out game configuration
+        self.DisplayCurrentGameState()
+
 
 
 game = BlackJackGame(2, 100)
 game.PlayBlackJack()
-game.Bet(110)
-
-print(game.player_money)
