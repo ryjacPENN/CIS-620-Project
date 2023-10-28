1aa6de0048ce2317821b762e137602146ff84a36
ryjacPENN <rjacobow@seas.upenn.edu>
2023-10-28 17:40:36 -0400

    Created game logic

--- Blackjack.py
+++ Blackjack.py
@@ -20,6 +20,8 @@ class BlackJackGame:
         self.deck = DeckCreator()
 
         self.player_cards = [[]] * num_players
+        self.player_hand_value = [] * num_players
+
         self.dealer_cards = []
         
         self.player_bets = [0] * num_players
@@ -44,18 +46,39 @@ class BlackJackGame:
         self.dealer_cards.append(self.deck.pop())
         self.dealer_cards.append(self.deck.pop())
 
+    def HandEvaluation(self, player):
+        value = 0
+        num_aces = 0
+
+        for card in self.player_cards[player]:
+            card_num = card[1]
+            if type(card_num) == int:
+                value += card_num
+            elif card_num == "J" or card_num == "Q" or card_num == "K":
+                value += 10
+            else:
+                num_aces += 1
+
+        for i in range(num_aces):
+            if (value + 11) <= 21:
+                value += 11
+            else:
+                value += 1
+
+        return value
+
     def Hit(self):
         self.player_cards[self.current_player].append(self.deck.pop())
 
     def Stand(self):
         self.current_player += 1
 
-    def Bet(self, amount):
-        curr_money = self.player_money[self.current_player] - amount
+    def Bet(self, amount, player):
+        curr_money = self.player_money[player] - amount
 
         if (curr_money >= 0):
-            self.player_bets[self.current_player] = amount
-            self.player_money[self.current_player] = curr_money
+            self.player_bets[player] = amount
+            self.player_money[player] = curr_money
             return True
         else:
             return False
@@ -69,23 +92,55 @@ class BlackJackGame:
 
         return card_name
 
-    def DisplayCurrentGameState(self):
+    def DisplayCurrentGameState(self, revealDealerCards):
         print("##################################")
         print("Turn", self.current_turn)
-        print("Dealer Cards :", self.DisplayCard(-1, 1), "and ?") # Hidden card is 0 index, revealed card is 1 index
+        print("Dealer Cards :", self.DisplayCard(-1, 1), "| ?") # Hidden card is 0 index, revealed card is 1 index
 
         for p in range(len(self.player_cards)):
-            print("Player", (p + 1), "Cards :", self.DisplayCard(p, 0), "and", self.DisplayCard(p, 1), ": $" + str(self.player_money[p]))
+            player_string = "Player " + str(p + 1) + " Cards: "
+            for c in range(len(self.player_cards[p])):
+                player_string += self.DisplayCard(p, c) + " | "
 
-        print("Player " + str(self.current_player + 1) + "'s turn: ")
+            player_string += "$" + str(self.player_money[p])
+            print(player_string)
 
     def PlayBlackJack(self):
-        # Start game to initial configuration
-        self.NextTurn()
+        while True:
+            # Go to next turn to initial configuration
+            self.NextTurn()
+            
+            # Print out game configuration
+            self.DisplayCurrentGameState()
+            
+            # Allow all players to bet
+            for p in range(len(self.player_bets)):
+                while True:
+                    bet_input = input("Enter Player " + str(p + 1) + "'s bet: ")
+                    if bet_input.isdigit():
+                        if (self.Bet(int(bet_input), p)):
+                            break
+                    
+                    print("INVALID BET")
 
-        # Print out game configuration
-        self.DisplayCurrentGameState()
+            # Allow all players to hit or stand
+            while True:
+                self.DisplayCurrentGameState()
 
+                curr_input = input("Player " + str(self.current_player + 1) + "'s turn: ").upper()
+
+                if curr_input == "HIT":
+                    self.Hit()
+                elif curr_input == "STAND":
+                    self.Stand()
+                elif curr_input == "END":
+                    print("END")
+                    return True
+                
+                if self.current_player > len(self.player_cards):
+                    break
+
+            
 
 
 game = BlackJackGame(2, 100)
