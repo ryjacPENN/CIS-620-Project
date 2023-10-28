b53b9d6b819846972ac496f4ce1d6a461d11a7a4
ryjacPENN <rjacobow@seas.upenn.edu>
2023-10-28 16:44:34 -0400

    Functions added

--- Blackjack.py
+++ Blackjack.py
@@ -15,13 +15,15 @@ def DeckCreator():
 
     return card_list
 
-class BlackjackDeck:
-    def __init__(self, num_players):
+class BlackJackGame:
+    def __init__(self, num_players, initial_money):
         self.deck = DeckCreator()
 
-        self.player_cards = [None] * num_players
-        self.player_bets = [None] * num_players
-        self.dealer_cards = [None] * num_players
+        self.player_cards = [[]] * num_players
+        self.dealer_cards = []
+        
+        self.player_bets = [0] * num_players
+        self.player_money = [initial_money] * num_players
 
         self.current_player = 0
 
@@ -29,7 +31,39 @@ class BlackjackDeck:
         self.deck = DeckCreator()
 
     def NextTurn(self):
-        pass
+        self.Shuffle()
+
+        for i in range(len(self.player_cards)):
+            self.player_cards[i] = []
+            self.player_cards[i].append(self.deck.pop())
+            self.player_cards[i].append(self.deck.pop())
+            
+        self.dealer_cards = []
+        self.dealer_cards.append(self.deck.pop())
+        self.dealer_cards.append(self.deck.pop())
 
     def Hit(self):
-        pass
+        self.player_cards[self.current_player].append(self.deck.pop())
+
+    def Stand(self):
+        self.current_player += 1
+
+    def Bet(self, amount):
+        curr_money = self.player_money[self.current_player] - amount
+
+        if (curr_money >= 0):
+            self.player_bets[self.current_player] = amount
+            self.player_money[self.current_player] = curr_money
+            return True
+        else:
+            return False
+
+    def PlayBlackJack(self):
+        self.NextTurn()
+
+
+game = BlackJackGame(2, 100)
+game.PlayBlackJack()
+game.Bet(110)
+
+print(game.player_money)
